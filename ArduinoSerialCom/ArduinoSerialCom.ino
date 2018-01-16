7e3deeb9e2d053c4c5f9fbe717405cb20a04f1a1
Martijn <martijn.dormans@gmail.com>
2018-01-16 10:52:11 +0100

    Add files via upload

---
+++ ArduinoSerialCom/ArduinoSerialCom.ino
@@ -0,0 +1,18 @@
+String GetSerialMessage() {
+  while (Serial.available() > 0 || ReadingSerial == true) {
+    if (Serial.available() > 0) {
+      String s = (String)(char) Serial.read();
+      if (s == "#" && !ReadingSerial) {
+        ReadingSerial = true;
+      }
+      else if (s == "%" && ReadingSerial) {
+        ReadingSerial = false;
+        return serialMessage;
+      }
+      else if (ReadingSerial) {
+        serialMessage += s;
+        //Serial.println(s);
+      }
+    }
+  }
+}
